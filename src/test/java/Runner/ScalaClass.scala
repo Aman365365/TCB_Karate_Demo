3aead9ee507530b9119815beea67d7e0629f9517
aman <125253064+Aman365365@users.noreply.github.com>
2023-11-09 20:57:04 +0530

    Update ScalaClass.scala

--- src/test/java/Runner/ScalaClass.scala
+++ src/test/java/Runner/ScalaClass.scala
@@ -38,27 +38,27 @@ class ScalaClass extends Simulation{
 
 
   // Run the Gatling simulation using the Karate feature
-  setUp(
-    createAndRead.inject(
-      nothingFor(10),
-      atOnceUsers(10),
-      rampUsers(users).during(rampUpDuration))
- ).protocols(protocol)
-    .maxDuration(1 minute)
+//  setUp(
+//    createAndRead.inject(
+ //     nothingFor(10),
+  //    atOnceUsers(10),
+  //    rampUsers(users).during(rampUpDuration))
+// ).protocols(protocol)
+ //   .maxDuration(1 minute)
 
- }
+// }
 
 // Run the Gatling simulation using the Karate feature
-//setUp(
-//createAndRead.inject(
-//nothingFor(4), // 1
-//atOnceUsers(2), // 2
-//rampUsers(1).during(5), // 3
-//constantUsersPerSec(1).during(5), // 4
+setUp(
+createAndRead.inject(
+nothingFor(4), // 1
+atOnceUsers(2), // 2
+rampUsers(1).during(5), // 3
+constantUsersPerSec(1).during(5), // 4
 //constantUsersPerSec(1).during(5).randomized, // 5
-//rampUsersPerSec(1).to(2).during(10.minutes), // 6
+rampUsersPerSec(1).to(2).during(10.minutes), // 6
 //rampUsersPerSec(10).to(20).during(10.minutes).randomized, // 7
-//stressPeakUsers(5).during(5) // 8
-//).protocols(protocol)
-//)
-//}
+//stressPeakUsers(1).during(5) // 8
+).protocols(protocol)
+)
+}
